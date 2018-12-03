74c5c13506d7bbc2c674405c6edd42776d724a2c
AditFadia <45273209+AditFadia@users.noreply.github.com>
2018-12-02 20:46:51 -0500

    Add files via upload

---
+++ Acitivty/MainActivity.cs
@@ -0,0 +1,84 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Database.Sqlite;
+using static Android.Widget.SeekBar;
+using System;
+using Android.Content;
+using XamarinQuizApp.Acitivty;
+
+namespace XamarinQuizApp
+{
+    [Activity(Label = "XamarinQuizApp", MainLauncher = false, Icon = "@drawable/icon",Theme ="@style/AppTheme")]
+    public class MainActivity : Activity,IOnSeekBarChangeListener
+    {
+        SeekBar seekBar;
+        TextView txtMode;
+        Button btnPlay, btnScore,btnlogout;
+        DbHelper.DbHelper db;
+        SQLiteDatabase sqliteDB;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView (Resource.Layout.Main);
+
+            db = new DbHelper.DbHelper(this);
+            sqliteDB = db.WritableDatabase;
+            btnlogout = FindViewById<Button>(Resource.Id.btnlogout);
+            seekBar = FindViewById<SeekBar>(Resource.Id.seekBar);
+            txtMode = FindViewById<TextView>(Resource.Id.txtMode);
+            btnPlay = FindViewById<Button>(Resource.Id.btnPlay);
+            btnScore = FindViewById<Button>(Resource.Id.btnScore);
+
+            seekBar.SetOnSeekBarChangeListener(this);
+            btnPlay.Click += delegate {
+                Intent intent = new Intent(this, typeof(Playing));
+                intent.PutExtra("MODE", getPlayMode());
+                StartActivity(intent);
+                Finish();
+            };
+            btnScore.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(Score));
+                StartActivity(intent);
+                Finish();
+            };
+            btnlogout.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(MainActivity1));
+                StartActivity(intent);
+                Finish();
+            };
+        }
+
+
+        private String getPlayMode()
+        {
+            if (seekBar.Progress == 0)
+                return Common.Common.MODE.EASY.ToString();
+            else if (seekBar.Progress == 1)
+                return Common.Common.MODE.MEDIUM.ToString();
+            else if (seekBar.Progress == 2)
+                return Common.Common.MODE.HARD.ToString();
+            else
+                return Common.Common.MODE.HARDEST.ToString();
+        }
+
+        public void OnProgressChanged(SeekBar seekBar, int progress, bool fromUser)
+        {
+          
+                txtMode.Text = getPlayMode().ToUpper();
+        }
+
+        public void OnStartTrackingTouch(SeekBar seekBar)
+        {
+         
+        }
+
+        public void OnStopTrackingTouch(SeekBar seekBar)
+        {
+           
+        }
+    }
+}
+
---
+++ Acitivty/MainActivity1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Database.Sqlite;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using XamarinQuizApp.Acitivty;
+using XamarinQuizApp.Common;
+using XamarinQuizApp.DbHelper;
+
+namespace XamarinQuizApp.Acitivty
+{
+    [Activity(MainLauncher = true)]
+    public class MainActivity1 : Activity
+    {
+
+        private EditText txtUsername, txtPassword;
+        private Button btnSignIn, btnCreate;
+        Helper helper;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main1);
+
+            txtUsername = FindViewById<EditText>(Resource.Id.txtusername);
+            txtPassword = FindViewById<EditText>(Resource.Id.txtpassword);
+            btnCreate = FindViewById<Button>(Resource.Id.btnSignUp);
+            btnSignIn = FindViewById<Button>(Resource.Id.btnSign);
+            helper = new Helper(this);
+
+      //      btnSignIn.Click += BtnSignIn_Click;
+            btnCreate.Click += delegate { StartActivity(typeof(SignUp)); };
+
+            btnSignIn.Click += delegate
+            {
+                try
+                {
+                    string Username = txtUsername.Text.ToString();
+                    string Password = txtPassword.Text.ToString();
+                    var user = helper.Authenticate(this, new Admin(null, Username, null, null, Password, null));
+                    if (user != null)
+                    {
+
+                        Toast.MakeText(this, "Login Successful", ToastLength.Short).Show();
+                        //  SetContentView(Resource.Layout.MainActvity);
+                        Intent intent = new Intent(this, typeof(MainActivity));
+                        StartActivity(intent);
+
+                    }
+                    else
+                    {
+                        Toast.MakeText(this, "Login Unsuccessful! Please verify your Username and Password", ToastLength.Short).Show();
+                    }
+                }
+                catch (SQLiteException ex)
+                {
+                    Toast.MakeText(this, "" + ex, ToastLength.Short).Show();
+                }
+
+            };
+        }
+        //private void BtnSignIn_Click(object sender, EventArgs e)
+        //{
+        //    SetContentView(Resource.Layout.SignUp);
+        //}
+        }
+}
